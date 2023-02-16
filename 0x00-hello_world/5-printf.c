10f9731df928539d619190de95408e491d890146
ngaharon <ngairaharon1@gmail.com>
2023-02-16 03:01:21 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A Program starts with the entry point
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
+
