daf0a8c812b1e56157cf0e2318f17a61cb013e0a
ngaharon <ngairaharon1@gmail.com>
2023-02-16 02:51:26 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program is a collection of functions
+ * Return: 0 (successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
