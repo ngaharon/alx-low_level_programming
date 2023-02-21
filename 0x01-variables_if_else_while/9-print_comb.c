afb8a18f4549067c6768b26924301ebf5fb3238c
ngaharon <ngairaharon1@gmail.com>
2023-02-21 15:34:30 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Retrun: Always 0 (success)
+ */
 
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		putchar(c + '0');
+		if (c < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
