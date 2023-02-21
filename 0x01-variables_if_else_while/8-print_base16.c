37ef67ddb3a9965c8a542fbbf4924389d3c3315e
ngaharon <ngairaharon1@gmail.com>
2023-02-21 14:58:04 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ */
+
+int main(void)
+{
+	char b;
+
+	int c;
+
+	b = 'a';
+	while
+		(b < 10) {
+			putchar(c + '0');
+			c++;
+		}
+	while
+		(b <= 'f') {
+			putchar(b);
+			b++;
+		}
+	putchar('\n');
+	return (0);
