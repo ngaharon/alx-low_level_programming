0f17e2d46d7d7d2d8c1e2cde3ab90d1f26698870
ngaharon <ngairaharon1@gmail.com>
2023-12-20 13:17:27 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,14 @@
 #include "main.h"
+
 /**
- * main -print _putchar
+ * main - function that prints _putchar
  * is written by mbah
  * Return: 0
  */
 
 int main(void)
 {
-	_putchr('_');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -16,5 +17,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,5 +1,5 @@
-#include "main.h"
-#include <unistd.>
+#include <unistd.h>
+
 /**
  * _putchar - writes the character cto stdout
  * @c: The character to print
