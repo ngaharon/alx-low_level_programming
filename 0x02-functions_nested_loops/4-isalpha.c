02e4bc5d996e800552ea089b399334da6ca605f8
ngaharon <ngairaharon1@gmail.com>
2023-12-20 14:31:50 -0800

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabets
+ * @c: function parameter
+ * Return: 1 and 0
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'z'))
+		return (1);
+	else
+		return (0);
+}
