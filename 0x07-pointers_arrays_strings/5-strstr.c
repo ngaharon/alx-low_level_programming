296aa758594034ff84fe77b7da83fab0058d4bca
ngaharon <ngairaharon1@gmail.com>
2024-01-09 10:12:16 -0800

    5. strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strstr - entry point
+ * @haystack: input
+ * @needle: input
+ * Return: always 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
+
+		while (*l == *p && *p != '\0')
+		{
+			l++;
+			p++;
+		}
+
+		if (*p == '\0')
+			return (haystack);
+	}
+	return (0);
+}
