cfbb79ac3b057a5dbff3a28db4641e0cb65802e7
ngaharon <ngairaharon1@gmail.com>
2024-01-09 07:17:09 -0800

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: always 0
+ */
+char *_strchr(char *s, char c)
+{
+	int l = 0;
+
+		for (; s[l] >= '\0'; l++)
+		{
+			if (s[l] == c)
+				return (&s[l]);
+		}
+	return (0);
+}
