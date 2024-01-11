2c5ca79bf484edf638a98e2851c541cc69bca07a
ngaharon <ngairaharon1@gmail.com>
2024-01-11 13:51:11 -0800

    Inception. Now, before you bother telling me it's impossible...

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -3,7 +3,7 @@
 /**
  * wildcmp - compare strings
  * @s1: pointer to string params
- * @s: pointer to string params
+ * @s2: pointer to string params
  * Return: 0
  */
 int wildcmp(char *s1, char *s2)
