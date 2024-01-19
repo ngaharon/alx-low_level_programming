09bccee66aff0dcece0027359e8309c5870dd1cb
ngaharon <ngairaharon1@gmail.com>
2024-01-19 04:56:45 -0800

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,25 +3,44 @@
 #include "main.h"
 /**
  * _strup - duplicate to new memory space location
- * @str: char
- * Return: 0
+ * @s1: string 1
+ * @s2: strng 2
+ * Return: concatenated string
  */
-char *_strdup(char *str)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *aaa;
+	unsigned int i;
+	unsigned int s1len = 0;
+	unsigned int s2len = 0;
+	char *output;
 
-	int i, r = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (i = 0; s1[i] != '\0'; i++)
+		s1len++;
+	for (i = 0; s2[i] != '\0'; i++)
+		s2len++;
 
-	if (str == NULL)
+	output = malloc(sizeof(char) * (s1len + n) + 1);
+	if (output == NULL)
 		return (NULL);
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	aaa = malloc(sizeof(char) * (i + 1));
-	if (aaa == NULL)
-		return (NULL);
-	for (r = 0; str[i]; r++)
-		aaa[i] = str[r];
-	return (aaa);
+	if (n >= s2len)
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+			output[i] = s1[i];
+		for (i = 0; s2[i] != '\0'; i++)
+			output[s1len + i] = s2[i];
+		output[s1len + i] = '\0';
+	}
+	else
+	{
+		for (i = 0; s1[i] != '\0'; i++)
+			output[i] = s1[i];
+		for (i = 0; i < n; i++)
+			output[s1len + i] = s2[i];
+		output[s1len + i] = '\0';
+	}
+	return (output);
 }
