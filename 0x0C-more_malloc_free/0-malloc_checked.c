f816bffa6d3c888f0d735f3feb25efd0334ee2fb
ngaharon <ngairaharon1@gmail.com>
2024-01-19 01:53:30 -0800

    0. Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 /**
  * create_array - create array of size and assign char c
@@ -6,18 +7,15 @@
  * @size: size of array
  * description: create array of size and assign char c
  * Return: pointer to array, NULL if fail
- *
  */
-char *create_array(unsigned int size, char c)
+
+void *malloc_checked(unsigned int b)
 {
-	char *str;
-	unsigned int i;
+	char *output;
 
-	str = malloc(sizeof(char) * size);
-	if (size == 0 || str == NULL)
-		return (NULL);
-
-	for (i = 0; i < size; i++)
-		str[i] = c;
-	return (str);
+	output = malloc(b);
+	if (output == NULL)
+		exit(98);
+	else
+		return (output);
 }
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	char *aaa;
+
+	int i, r = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	aaa = malloc(sizeof(char) * (i + 1));
+	if (aaa == NULL)
+		return (NULL);
+	for (r = 0; str[i]; r++)
+		aaa[i] = str[r];
+	return (aaa);
+}
