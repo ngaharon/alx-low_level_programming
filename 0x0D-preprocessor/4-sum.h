f4cab157ff044a4b8fa447c52f2716b8a57ff3dd
ngaharon <ngairaharon1@gmail.com>
2024-01-22 14:09:34 -0800

    4. SUM

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,17 +1,6 @@
-#include <stdio.h>
-#include "4-sum.h"
-#include "4-sum.h"
+#ifndef SUM_H
+#define SUM_H
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int s;
+#define SUM(x, y) ((x) + (y))
 
-    s = SUM(98, 1024);
-    printf("%d\n", s);
-    return (0);
-}
+#endif
