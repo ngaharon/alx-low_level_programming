2b229b646eb9587e3cfde0f65cd8db6aaec19a83
ngaharon <ngairaharon1@gmail.com>
2024-02-15 09:30:20 -0800

    Speak gently, she can hear

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <font1.h>
+#include <fcntl.h>
 #include <unistd.h>
 
 int _putchar(char c);
@@ -13,6 +13,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _strlen(char *str);
-int append_text_to_file(const chaer *filename, chaer *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif
